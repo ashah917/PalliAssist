4c9ab335a01143732665d7d03dec1d6daabaf8b4
Steven <sat2@rice.edu>
2016-12-03 16:18:40 -0600

    menu skeleton

--- PalliAssistMobile/app/src/main/java/io/palliassist/palliassistmobile/MainActivity.java
+++ PalliAssistMobile/app/src/main/java/io/palliassist/palliassistmobile/MainActivity.java
@@ -7,21 +7,54 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 import android.view.MenuItem;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
 import io.palliassist.palliassistmobile.twilio.ui.ChatActivity;
 
 public class MainActivity extends FragmentActivity {
-    public static final String EXTRA_IMAGE = "extra_image";
 
-    private int extraCurrentItem;
-    private ViewPager mPager;
+    Button button_message;
+    Button button_notify;
+    Button button_emergency;
+    Button button_settings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Set current item based on the extra passed in the activity
-        extraCurrentItem = getIntent().getIntExtra(EXTRA_IMAGE, -1);
+        button_message = (Button)findViewById(R.id.button_message);
+        button_notify = (Button)findViewById(R.id.button_notify);
+        button_emergency = (Button)findViewById(R.id.button_emerg);
+        button_settings = (Button)findViewById(R.id.button_setting);
+
+    }
+
+
+    /**
+     * button listener that will do something if specific button is clicked
+     *
+     */
+
+    public void buttonClicked(View view) {
+        if (view.getId() == R.id.button_message) {
+            final Intent i = new Intent(this, ChatActivity.class);
+            startActivity(i);
+            finish();
+        }
+        if (view.getId() == R.id.button_notify) {
+            /* TODO*/
+            return;
+        }
+        if (view.getId() == R.id.button_emerg) {
+            /*TODO*/
+            return;
+        }
+        if (view.getId() == R.id.button_setting) {
+            /* TODO*/
+            return;
+        }
 
     }
 
@@ -31,14 +64,21 @@ public class MainActivity extends FragmentActivity {
      * @return
      */
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onContextItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             //Chat option(located in res/menu/main_menu.xml)
-            case R.id.discuss:
+            case R.id.context_message:
                 final Intent i = new Intent(this, ChatActivity.class);
-                i.putExtra(MainActivity.EXTRA_IMAGE, extraCurrentItem);
                 startActivity(i);
                 finish();
+                return true;
+            //Other 3 context menus need to be completed
+            case R.id.context_notify:
+                return true;
+            case R.id.context_emerg:
+                return true;
+            case R.id.context_setting:
+                return true;
         }
         return super.onOptionsItemSelected(item);
     }
